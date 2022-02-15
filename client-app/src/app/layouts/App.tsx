0a52767c76618804bd403cce4bb80608ecb56bcf
Ivana Murray <ivana.murray@parasemper.com>
2022-02-15 10:25:03 +0200

    End of Section 6

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,70 +1,98 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from '../layouts/NavBar';
+import LoadingComponents from '../layouts/LoadingComponents';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
+import agent from '../api/agent';
 
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
+    const [activities, setActivities] = useState<Activity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+    const [editMode, setEditMode] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [submitting, setSubmitting] = useState(false);
 
-  useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
-      setActivities(response.data);
-    })
-  }, []);
+    useEffect(() => {
+        agent.Activities.list().then((response) => {
+            let activities: Activity[] = [];
+            response.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                activities.push(activity);
+            })
+            setActivities(activities);
+            setLoading(false);
+        })
+    }, []);
 
-  function handleSelectActivity(id : string) {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
+    function handleSelectActivity(id: string) {
+        setSelectedActivity(activities.find(x => x.id === id));
+    }
 
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined);
-  }
+    function handleCancelSelectActivity() {
+        setSelectedActivity(undefined);
+    }
 
-  function handleFormOpen(id? : string ) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
+    function handleFormOpen(id?: string) {
+        id ? handleSelectActivity(id) : handleCancelSelectActivity();
+        setEditMode(true);
+    }
 
-  function handleFormClose() {
-    setEditMode(false);
-  }
+    function handleFormClose() {
+        setEditMode(false);
+    }
 
-  function handleEditOrAddActivity(activity: Activity) {
-    activity.id 
-      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-      : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
-  }
+    function handleEditOrAddActivity(activity: Activity) {
+        setSubmitting(true);
+        if (activity.id) {
+            agent.Activities.update(activity).then(() => {
+                setActivities([...activities.filter(x => x.id !== activity.id), activity]);
+                setEditMode(false);
+                setSelectedActivity(activity);
+                setSubmitting(false);
+            })
+        } else {
+            activity.id = uuid();
+            agent.Activities.create(activity).then(() => {
+                setActivities([...activities, activity ])
+                setEditMode(false);
+                setSelectedActivity(activity);
+                setSubmitting(false);
+            })
+        }
+    }
 
-  function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter( x => x.id !== id)])
-  }
+    function handleDeleteActivity(id: string) {
+        setSubmitting(true);
+        agent.Activities.delete(id).then(() => {
+            setSubmitting(false);
+        })
+        setActivities([...activities.filter(x => x.id !== id)])
+    }
 
-  return (
-    <>
-      <NavBar openForm={handleFormOpen}/>
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEdit={handleEditOrAddActivity}
-          deleteActivity={handleDeleteActivity}
-        />
-      </Container>
-    </> 
-  );
+    if (loading) return <LoadingComponents content='Loading App' />
+
+    return (
+        <>
+            <NavBar openForm={handleFormOpen} />
+            <Container style={{ marginTop: '7em' }}>
+                <ActivityDashboard
+                    activities={activities}
+                    selectedActivity={selectedActivity}
+                    selectActivity={handleSelectActivity}
+                    cancelSelectActivity={handleCancelSelectActivity}
+                    editMode={editMode}
+                    openForm={handleFormOpen}
+                    closeForm={handleFormClose}
+                    createOrEdit={handleEditOrAddActivity}
+                    deleteActivity={handleDeleteActivity}
+                    submitting = {submitting}
+                />
+            </Container>
+        </>
+    );
 }
 
 export default App;
