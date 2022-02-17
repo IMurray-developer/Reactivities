2427787a478aa356428b4055d314e679f9f3df42
Ivana Murray <ivana@clientsavvy.com>
2022-02-17 13:02:47 +0200

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
-import ActivityDetails from '../Details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponents from '../../../app/layouts/LoadingComponents';
 
 export default observer(function ActivityDashboard() {
-
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
+    const {loadActivities, activityRegistry } = activityStore;
+
+    useEffect(() => {
+        if (activityRegistry.size <= 0) loadActivities();
+    }, [activityRegistry.size, loadActivities]);
+
+    if (activityStore.loadingInitial) return <LoadingComponents content='Loading App' />
+
 
     return (
         <Grid>
@@ -18,10 +22,7 @@ export default observer(function ActivityDashboard() {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width={6}>
-                {selectedActivity && !editMode &&
-                    <ActivityDetails />}
-                {editMode &&
-                    <ActivityForm />}
+                <h2>Activity filters</h2>
             </Grid.Column>
         </Grid>
     )
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'semantic-ui-css/semantic.min.css';
 import './app/layouts/styles.css';
-import App from './app/layouts/App'; 
+import App from './app/layouts/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
-        <App /> 
-    </StoreContext.Provider> ,
-  document.getElementById('root')
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StoreContext.Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
